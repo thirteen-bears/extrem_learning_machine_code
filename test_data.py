4a5a287c3c37296ac3e33fbd986fb13ec3ccd2e4
Han <hu587@purdue.edu>
2021-05-09 20:24:21 -0400

    Update test_data.py

--- test_data.py
+++ test_data.py
@@ -23,13 +23,14 @@ class HiddenLayer:
         row = x.shape[0]
         columns = x.shape[1]
         rnd = np.random.RandomState(4444)
-        self.w = rnd.uniform(-1, 1, (columns, num))  #
-        self.b = np.zeros([row, num], dtype=float)  # 随机设定隐含层神经元阈值，即bi的值
+        self.w = rnd.uniform(-1, 1, (columns, num))  # [columns, num][5,90]
+        self.b = np.zeros([row, num], dtype=float)  # 随机设定隐含层神经元阈值，即bi的值 [(3647, 90)]
         for i in range(num):
             rand_b = rnd.uniform(-0.4, 0.4)  # 随机产生-0.4 到 0.4 之间的数
             for j in range(row):
                 self.b[j, i] = rand_b  # 设定输入层与隐含层的连接权值
         self.h = self.sigmoid(np.dot(x, self.w) + self.b)  # 计算隐含层输出矩阵H
+        #(3647, 5) *[5,90] + 3647, 90
         self.H_ = np.linalg.pinv(self.h)  # 获取H的逆矩阵
         # print(self.H_.shape)
  
@@ -118,32 +119,12 @@ y_test = dt_02.iloc[:,[6]]
 scaler = StandardScaler()
 x_train_1 = scaler.fit_transform(x_train.values[:,:])
 y_train_1 = scaler.fit_transform(y_train.values[:,:])
-x_test_1 = scaler.fit_transform(x_test.values[:,:])
-y_test_1 = scaler.fit_transform(y_test.values[:,:])
+#x_test_1 = scaler.fit_transform(x_test.values[:,:])
+#y_test_1 = scaler.fit_transform(y_test.values[:,:])
+x_test_1 = np.asarray(x_test.values[:,:],'float64')
+y_test_1 = np.asarray(y_test.values[:,:],'float64')
 
-'''
-for i in range(x_train.shape[1]):
-    _min = np.min(x_train[:, i])  # 每一列的最小值
-    _max = np.max(x_train[:, i])  # 每一列的最大值
-    x_train[:, i] = (x_train[:, i] - _min) / (_max - _min)  # 归一化到0-1之间
-
-for i in range(y_train.shape[1]):
-    _min = np.min(y_train[:, i])  # 每一列的最小值
-    _max = np.max(y_train[:, i])  # 每一列的最大值
-    y_train[:, i] = (y_train[:, i] - _min) / (_max - _min)  # 归一化到0-1之间
-
-for i in range(x_test.shape[1]):
-    _min = np.min(x_test[:, i])  # 每一列的最小值
-    _max = np.max(x_test[:, i])  # 每一列的最大值
-    x_test[:, i] = (x_test[:, i] - _min) / (_max - _min)  # 归一化到0-1之间
-    
-for i in range(y_test.shape[1]):
-    _min = np.min(y_test[:, i])  # 每一列的最小值
-    _max = np.max(y_test[:, i])  # 每一列的最大值
-    y_test[:, i] = (y_test[:, i] - _min) / (_max - _min)  # 归一化到0-1之间
-'''
-
-a = HiddenLayer(x_train_1, 90)  # data_X
+a = HiddenLayer(x_train_1, 1000)  # data_X
 a.regressor_train(y_train_1)
  
 result = a.regressor_test(x_test_1)
@@ -154,8 +135,8 @@ for i in result:
     ground_truth = y_test_1[j]  # 真实值
     # if infer_result-ground_truth>0 or infer_result-ground_truth<0:
     #     infer_result = ground_truth
-    #cost = np.power(infer_result - ground_truth, 2)/ground_truth
-    cost = np.abs(infer_result - ground_truth)/ground_truth
+    cost = np.power(infer_result - ground_truth, 2)
+    #cost = np.abs(infer_result - ground_truth)/ground_truth
     sum_cost += cost
     j += 1
 average_cost = sum_cost/j
